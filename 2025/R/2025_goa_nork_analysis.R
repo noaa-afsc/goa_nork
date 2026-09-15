5f806eda8d3077cd4eff7e513a7634284b76288b
Ben Williams <ben.williams@noaa.gov>
2026-09-15 08:49:00 -0800

    2025 setup

---
+++ 2025/R/2025_goa_nork_analysis.R
@@ -0,0 +1,14 @@
+# note: the govt was shut down during the 2025 assessment cycle
+# this assessment was delayed until 2026
+
+library(afscdata)
+# devtools::unload('afscassess')
+library(afscassess)
+
+setup_folders(2026)
+accepted_model(base_year=2024, base_model='m24', year=2026)
+
+
+
+
+
